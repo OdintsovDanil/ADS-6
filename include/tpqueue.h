081ffae01bc959a44f6e0d0142be977f9b401113
OdintsovDanil <99181188+OdintsovDanil@users.noreply.github.com>
2022-04-16 23:32:36 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,32 +5,32 @@
 template<typename T, int size>
 class TPQueue {
  private:
-	T arr[5];
-	int first;
-	int last;
+    T arr[5];
+    int first;
+    int last;
 
  public:
-	TPQueue() {
-		first = 0;
-		last = 0;
-	}
-	void push(T elem) {
-		if (last - first >= size) {
-			cout << "Full!" << endl;
-		} else {
-			int iter = last++;
-			for (iter; (--iter >= first) && (arr[iter % size].prior < elem.prior);) {
-				arr[(iter + 1) % size] = arr[iter % size];
-			}
-			arr[(iter + 1) % size] = elem;
-		}
-	}
-	T pop() {
-		return arr[(first++) % size];
-	}
+    TPQueue() {
+        first = 0;
+        last = 0;
+    }
+    void push(T elem) {
+        if (last - first >= size) {
+            cout << "Full!" << endl;
+        } else {
+            int iter = last++;
+            for (iter; (--iter >= first) && (arr[iter % size].prior < elem.prior);) {
+                arr[(iter + 1) % size] = arr[iter % size];
+            }
+            arr[(iter + 1) % size] = elem;
+        }
+    }
+    T pop() {
+        return arr[(first++) % size];
+    }
 };
 
-struct SYM {
+struct SYM { 
   char ch;
   int prior;
 };
